00343b96914175a9e0b0b614e2b19b9eaa57209b
kisoji <kotooka149@yahoo.co.jp>
2020-12-12 18:33:20 +0900

    Revert "Merge pull request #9 from kisoji/second_training"
    This reverts commit f57f6b3b64a2d18d083f2c9852b82c4035b2769c, reversing
    changes made to 2c5e5ec77102e9db46c6c665e1b01695883d4af9.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,19 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-        # wdayメソッドを用いて取得した数値
-        wday_num = Date.today.wday 
-
-      if @todays_date.wday + x > 7
-       
-        wday_num = @todays_date.wday + x - 7
-      else
-        wday_num = @todays_date.wday + x  
-      
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date.day + x), plans: today_plans, wday: wdays[wday_num]}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans}
       @week_days.push(days)
     end
 
